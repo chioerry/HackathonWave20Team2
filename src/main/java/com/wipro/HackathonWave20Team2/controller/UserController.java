06eed2a3b98f0c0f29c7ffceb25670596bf9a91c
spraju92 <spraju92@gmail.com>
2017-10-16 12:47:55 +0530

    up to CRUD operations

--- src/main/java/com/wipro/HackathonWave20Team2/controller/UserController.java
+++ src/main/java/com/wipro/HackathonWave20Team2/controller/UserController.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wipro.HackathonWave20Team2.domain.UserBean;
 import com.wipro.HackathonWave20Team2.service.UserService;
-
+@RestController
 public class UserController {
 	@Autowired
 	UserService userService;
--- src/main/java/com/wipro/HackathonWave20Team2/domain/UserBean.java
+++ src/main/java/com/wipro/HackathonWave20Team2/domain/UserBean.java
@@ -21,46 +21,54 @@ public class UserBean {
 	}
 
 	// Parameterised Constructor if you want to set values using a constructor
-	public UserBean(String id, String userName, int userAge, String address, String emailId) {
+	public UserBean(String id, String userName, int age, String address, String emailId) {
 		super();
 		this.id = id;
 		this.userName = userName;
-		this.age = userAge;
+		this.age = age;
 		this.address = address;
 		this.emailId = emailId;
 	}
 	
 	// Setter and Getter methods if you want to set values individually
-	
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	public int getUserAge() {
+
+	public int getAge() {
 		return age;
 	}
-	public void setUserAge(int userAge) {
-		this.age = userAge;
+
+	public void setAge(int age) {
+		this.age = age;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getEmailId() {
 		return emailId;
 	}
+
 	public void setEmailId(String emailId) {
 		this.emailId = emailId;
 	}
-
+	
 }
